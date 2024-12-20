65a01eb717fd48a299b6f7d5bffede4478e5cdff
jackson212 <jacksongerge770@gmail.com>
2024-12-20 09:41:07 -0500

    added nat gate ways and allocated to public subnet for internet acces

--- RouteTables.tf
+++ RouteTables.tf
@@ -1,9 +1,13 @@
 
 
 resource "aws_route_table" "Public RT" {
-  count = length(var.public_subnet_cidr_blocks)
+  
 
   vpc_id = aws_vpc.default.id
+   route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.public_internet_gateway.id
+  }
 
   tags = merge(
     {
@@ -18,16 +22,33 @@ resource "aws_route_table" "Public RT" {
 
 
 
-resource "aws_route_table" "Private RT" {
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.myVPC.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.NAT-Gateway.id
+  }
+
+  tags = {
+    Name = "Private Route Table"
+  }
+}
+
+
+
+resource "aws_route_table_association" "private" {
   count = length(var.private_subnet_cidr_blocks)
 
-  vpc_id = aws_vpc.default.id
-
-  tags = merge(
-    {
-      Name        = "PrivateRouteTable",
-      Project     = var.project,
-  
-    },
-  )  
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
 }
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidr_blocks)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+
